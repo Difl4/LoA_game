84e1625dd92f4066c4e1d1a0f7e155ee06ce3d80
David Almeida Mota Pereira <up202405163@up.pt>
2025-04-05 21:14:02 +0100

    Delete Projeto directory

--- Projeto/ai/base_ai.py
+++
@@ -1,7 +0,0 @@
-class BaseAI():
-    """Interface all AIs must follow."""
-    def __init__(self, game, color):
-        self.game = game
-        self.color = color
-        self.search_depth = 3
-        self.nodes_explored = 0
